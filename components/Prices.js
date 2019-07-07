2a00974d03bb775404f59e7bb74a3316ad833238
Venkata Kaushik Vemani <venkatakaushik1@gmail.com>
2019-07-07 23:04:05 +0530

    Course completed

---
+++ components/Prices.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+class Prices extends Component {
+    state = {
+        currency : 'USD'
+    }
+    render() {
+        let list = '';
+
+        if(this.state.currency == 'USD') {
+            list = <li className="list-group-item">
+                        Bitcoin rate for {this.props.bpi.USD.description}
+                        : <span className="badge badge-primary">{this.props.bpi.USD.code}</span>
+                        <strong>{this.props.bpi.USD.rate}</strong>
+                    </li>
+        } else if(this.state.currency == 'GBP') {
+            list = <li className="list-group-item">
+            Bitcoin rate for {this.props.bpi.GBP.description}
+            : <span className="badge badge-primary">{this.props.bpi.GBP.code}</span>
+            <strong>{this.props.bpi.GBP.rate}</strong>
+        </li>
+        } else if(this.state.currency == 'EUR') {
+            list = <li className="list-group-item">
+            Bitcoin rate for {this.props.bpi.EUR.description}
+            : <span className="badge badge-primary">{this.props.bpi.EUR.code}</span>
+            <strong>{this.props.bpi.EUR.rate}</strong>
+        </li>
+        }
+        return (
+            <div>
+                <ul className="list-group">
+                    {list}
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default Prices;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,21 @@
+import fetch from 'isomorphic-unfetch';
 import Layout from '../components/Layout'
-
-const Index = () => (
+import Prices from '../components/Prices'
+const Index = (props) => (
     <Layout>
         <div>
-            <h1>Welcome to Bitcoins Prices</h1>    
+            <h1>Welcome to Bitcoin Prices</h1> 
+            <Prices bpi={props.bpi}/>
         </div>
     </Layout>
 )
 
+Index.getInitialProps = async function() {
+    const res = await fetch(
+        'https://api.coindesk.com/v1/bpi/currentprice.json');
+    const data = await res.json()
+    return {
+        bpi : data.bpi
+    }
+}
 export default Index;
